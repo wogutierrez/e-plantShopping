168e3ab5514b8bafb377895eaa8b8c98e6d3049e
william <william@gmail.com>
2026-03-10 16:47:16 +0200

    Auto-commit: src/CartItem.jsx at 3/10/2026, 4:47:16 PM

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -33,11 +33,11 @@ const CartItem = ({ onContinueShopping }) => {
   };
 
   const handleDecrement = (item) => {
-    if (item.quantity >= 2) {
+    if (item.quantity >= 1) {
       dispatch(
         updateQuantity({ name: item.name, quantity: item.quantity - 1 })
       );
-    } else if (item.quantity === 1) {
+    } else if (item.quantity === 0) {
       dispatch(
         updateQuantity({ name: item.name, quantity: item.quantity - 1 })
       );
