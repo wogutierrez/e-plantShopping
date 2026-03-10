cf0da528cffd43a0f02ac7a49a132a2e5124d745
wogutierrez@gmail.com <wogutierrez@gmail.com>
2026-03-10 21:24:25 +0200

    Auto-commit: src/CartSlice.jsx at 3/10/2026, 9:24:25 PM

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -8,7 +8,7 @@ export const CartSlice = createSlice({
   reducers: {
     addItem: (state, action) => {
       const { name, image, cost } = action.payload;
-      const existingItem = state.items.find((item) => item.name === name);
+      const existingItem = state.items.finds((item) => item.name === name);
       if (existingItem) {
         existingItem.item.updateQuantity++;
       } else {
