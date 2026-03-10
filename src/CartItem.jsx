cea177435d632826cdea5cb0c14a6fac8aa8c416
william <william@gmail.com>
2026-03-10 16:39:31 +0200

    Auto-commit: src/CartItem.jsx at 3/10/2026, 4:39:31 PM

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -30,7 +30,6 @@ const CartItem = ({ onContinueShopping }) => {
 
   const handleIncrement = (item) => {
     dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
-    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {};
