5d7b543b0ce20cd4aaf402d387cc400a4ef05509
william <william@gmail.com>
2026-03-11 09:04:18 +0200

    Auto-commit: src/ProductList.jsx at 3/11/2026, 9:04:18 AM

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -11,6 +11,10 @@ function ProductList({ onHomeClick }) {
   const dispatch = useDispatch();
   const cartItems = useSelector((state) => state.cart.items);
 
+  const handleAddToCart = (product) => {
+    dispatch(addItem(product));
+  };
+
   const plantsArray = [
     {
       category: "Air Purifying Plants",
@@ -336,9 +340,9 @@ function ProductList({ onHomeClick }) {
                     d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8"
                     fill="none"
                     stroke="#faf9f9"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     id="mainIconPathAttribute"
                   ></path>
                 </svg>
